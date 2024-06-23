3ea47ffbaab1b4f883fce24620ee2cb87d298f71
Qing <yoimiya114@163.com>
2024-06-23 17:11:01 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-icon size="size" color="color">
+      <Edit />
+    </el-icon>
+    <el-icon><Plus /></el-icon>
+  </div>
 </template>
 
 <style lang="less">
