34178b8b4701b90fdac383c7c3c0ec518ec27161
Qing <yoimiya114@163.com>
2024-06-23 19:46:29 +0800

    首页布局初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    主页面
-    <el-button @click="logout">退出</el-button>
-  </div>
+    <el-container>
+      <el-header>
+        <h3>电商后台管理系统</h3>
+        <el-button @click="logout">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+
+        </el-aside>
+        <el-container>
+          <el-main>
+
+          </el-main>
+          <el-footer>
+
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -16,3 +31,29 @@ export default {
   },
 }
 </script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-header{
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+    .el-button{
+      margin: 15px 0 15px 0;
+    }
+  }
+  .el-aside{
+    background-color: #373d41;
+    height: 100%;
+  }
+  .el-main{
+    background-color: #eaedf1;
+    height: 100%;
+  }
+  .el-footer{
+    background-color: #373d41;
+  }
+}
+</style>
