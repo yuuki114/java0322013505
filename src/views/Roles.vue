c50fc6f0df89b4d7fe4958b0a56cf993ab588f46
Qing <yoimiya114@163.com>
2024-06-24 14:48:53 +0800

    实现编辑角色功能

--- src/views/Roles.vue
+++ src/views/Roles.vue
@@ -7,7 +7,7 @@
     </el-breadcrumb>
     <el-card class="box-card">
       <el-row>
-          <el-button class="btn-addRole" type="primary" @click="addRoleDialogVisible = true">添加角色</el-button>
+        <el-button class="btn-addRole" type="primary" @click="addRoleDialogVisible = true">添加角色</el-button>
       </el-row>
       <el-table :data="roleList" border style="width: 100%">
         <el-table-column prop="id" label="#" width="180"/>
@@ -15,9 +15,9 @@
         <el-table-column prop="roleDesc" label="角色描述"/>
         <el-table-column label="操作">
           <template #default="scope">
-              <el-button type="primary" icon="Edit" @click="showEditUser(scope.row)">编辑</el-button>
-              <el-button type="danger" icon="Delete" @click="delUser(scope.row.id)">删除</el-button>
-              <el-button type="warning" icon="Setting" @click="showSetRole(scope.row)">分配权限</el-button>
+            <el-button type="primary" icon="Edit" @click="showEditRole(scope.row)">编辑</el-button>
+            <el-button type="danger" icon="Delete" @click="delRole(scope.row.id)">删除</el-button>
+            <el-button type="warning" icon="Setting" @click="showSetRole(scope.row)">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -43,6 +43,27 @@
         </div>
       </template>
     </el-dialog>
+
+    <el-dialog
+        v-model="editRoleDialogVisible"
+        title="编辑权限"
+        width="40%"
+    >
+      <el-form class="addRoleForm" :model="roleInfo" label-width="120px">
+        <el-form-item label="角色名称">
+          <el-input size="large" prefix-icon="UserFilled" v-model="roleInfo.roleName"/>
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input size="large" prefix-icon="Tickets" v-model="roleInfo.roleDesc"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="editRoleDialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="editRole">确认</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -50,10 +71,11 @@
 import {ElMessage} from "element-plus";
 
 export default {
-  data(){
-    return{
+  data() {
+    return {
       roleList: [],
       addRoleDialogVisible: false,
+      editRoleDialogVisible: false,
       roleInfo: {
         roleName: '',
         roleDesc: ''
@@ -63,13 +85,13 @@ export default {
   created() {
     this.getRoleList();
   },
-  methods:{
-    getRoleList(){
-      this.$axios.get('roles').then(res=>{
+  methods: {
+    getRoleList() {
+      this.$axios.get('roles').then(res => {
         this.roleList = res.data.data;
       })
     },
-    addRole(){
+    addRole() {
       this.$axios.post('roles', this.roleInfo).then((res) => {
         if (res.data.meta.status === 201) {
           ElMessage.success('添加角色成功');
@@ -79,20 +101,36 @@ export default {
         }
       })
       this.addRoleDialogVisible = false;
-    }
+    },
+    showEditRole(roleInfo) {
+      this.roleInfo = roleInfo;
+      this.editRoleDialogVisible = true;
+    },
+    editRole() {
+      this.$axios.put('roles/' + this.roleInfo.id, this.roleInfo).then((res) => {
+        if (res.data.meta.status === 200) {
+          ElMessage.success('编辑角色成功');
+          this.getRoleList();
+        } else {
+          ElMessage.error('编辑角色失败：' + res.data.meta.msg);
+        }
+      })
+      this.editRoleDialogVisible = false;
+    },
   }
 }
 </script>
 
 <style scoped lang="less">
-.box-card{
+.box-card {
   margin-top: 20px;
-  .btn-addRole{
+
+  .btn-addRole {
     margin-bottom: 10px;
   }
 }
 
-.addRoleForm{
+.addRoleForm {
   .el-form {
     display: flex;
     flex-direction: column;
